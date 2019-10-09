6506944d748a64bd97ea6170906f514503a8976d
Tomás <truizlop@gmail.com>
2019-10-09 10:03:12 +0200

    Renames

--- Tests/MetaTests/Extensions/Snapshotting+Codegen.swift
+++ Tests/MetaTests/Extensions/Snapshotting+Codegen.swift
@@ -5,19 +5,17 @@ import Meta
 import BowEffects
 
 extension Snapshotting where Value == URL, Format == String {
-    static func generatedCode<T: CodegenVisitor>(_ visitor: T) -> Snapshotting<URL, String> {
-        var strategy = Snapshotting<String, String>.lines.pullback { (url: URL) -> String in
+    static func generatedCode<T: CodegenVisitor>(visitor: T) -> Snapshotting<URL, String> {
+        Snapshotting<String, String>.lines.pullback { (url: URL) -> String in
             let tree = try! SyntaxTreeParser.parse(url)
             tree.walk(visitor)
             return visitor.generatedCode
         }
-        strategy.pathExtension = "swift"
-        return strategy
     }
     
-    static let copyMethod: Snapshotting<URL, String> = .generatedCode(CopyVisitor())
+    static let copyMethod: Snapshotting<URL, String> = .generatedCode(visitor: CopyVisitor())
     
-    static func copyGeneration<D: CodegenDependencies>(for environment: D) -> Snapshotting<URL, String> {
+    static func generatedCode<D: CodegenDependencies>(using environment: D) -> Snapshotting<URL, String> {
         var strategy = Snapshotting<String, String>.lines.pullback { (url: URL) -> String in
             try! generateCode(forFilesIn: url.path)
                 .provide(environment)
