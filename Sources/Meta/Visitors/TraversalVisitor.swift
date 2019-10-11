d14afbeca8899c3429bcb8a14dd61e98044b73c8
Tomás <truizlop@gmail.com>
2019-10-11 13:20:03 +0200

    Extract duplicated code

--- Sources/Meta/Visitors/TraversalVisitor.swift
+++ Sources/Meta/Visitors/TraversalVisitor.swift
@@ -42,21 +42,3 @@ public class TraversalVisitor: SyntaxVisitor, CodegenVisitor {
         """
     }
 }
-
-fileprivate extension String {
-    var nonArray: String {
-        if hasPrefix("[") {
-            return String(dropFirst().dropLast())
-        } else if hasPrefix("Array<") {
-            return String(replacingOccurrences(of: "Array<", with: "").dropLast())
-        } else if hasPrefix("ArrayK<") {
-            return String(replacingOccurrences(of: "ArrayK<", with: "").dropLast())
-        } else if hasPrefix("NEA<") {
-            return String(replacingOccurrences(of: "NEA<", with: "").dropLast())
-        } else if hasPrefix("NonEmptyArray<") {
-            return String(replacingOccurrences(of: "NonEmptyArray<", with: "").dropLast())
-        } else {
-            return self
-        }
-    }
-}
