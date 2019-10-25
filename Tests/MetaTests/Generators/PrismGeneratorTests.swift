737c80f0d1f25362fe9dc2a554983e26607d13a8
Miguel Ángel Díaz <madiaz.dev@gmail.com>
2019-10-25 13:43:59 +0200

    snapshot test for prism generator

--- Tests/MetaTests/Generators/PrismGeneratorTests.swift
+++ Tests/MetaTests/Generators/PrismGeneratorTests.swift
@@ -3,7 +3,6 @@ import SnapshotTesting
 import Meta
 
 class PrismGeneratorTests: XCTestCase {
-    
     func testPrismGeneratorMultipleFile() {
         assertSnapshot(matching: URL.meta.fixtures, as: .generatedCode(using: PrismGenerator()))
     }
